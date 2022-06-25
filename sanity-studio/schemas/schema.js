45cb941cb41f2a8c37895923477592d9cce3d948
Carlos Marchal <marchal.carlos@pm.me>
2022-06-25 19:09:42 +0200

    added category schema

--- sanity-studio/schemas/schema.js
+++ sanity-studio/schemas/schema.js
@@ -3,8 +3,9 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import siteSetting from "./site-settings";
 import homePage from "./home";
+import category from "./category";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([siteSetting, homePage]),
+  types: schemaTypes.concat([siteSetting, homePage, category]),
 });
