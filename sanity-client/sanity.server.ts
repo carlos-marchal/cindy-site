7521eeabe1c6adcfb0c91948adb7beb70885ee2f
Carlos Marchal <marchal.carlos@pm.me>
2022-06-26 21:17:39 +0200

    augmented fetched sanity images at build time with a blurred base64 placeholder

--- sanity-client/sanity.server.ts
+++ sanity-client/sanity.server.ts
@@ -2,11 +2,14 @@ import { GetStaticPropsResult } from "next";
 import { createClient } from "next-sanity";
 import {
   getSanityData,
+  isSanityImageReference,
   sanityConfig,
+  SanityImageReference,
   SanityProps,
   sanitySettingsQuery,
   SiteSettings,
 } from "./config";
+import { urlBuilder } from "./sanity";
 
 const sanityClient = createClient(sanityConfig);
 
@@ -30,6 +33,43 @@ export async function getSanityStaticProps<T extends unknown[]>(
   if (responses.every((response) => response === undefined)) {
     return { notFound: true };
   }
-  const data = responses.map((response) => getSanityData(response, preview));
+  const data = await Promise.all(
+    responses.map(async (response) => {
+      const entry = getSanityData(response, preview);
+      await loadSanityBlurredPlaceholders(entry);
+      return entry;
+    })
+  );
   return { props: { queries, data: data as [SiteSettings, ...T], preview } };
+}
+
+async function loadSanityBlurredPlaceholders(
+  data: unknown | SanityImageReference
+): Promise<void> {
+  if (typeof data !== "object" || data === null) {
+    return;
+  }
+  if (Array.isArray(data)) {
+    await Promise.all(data.map(loadSanityBlurredPlaceholders));
+    return;
+  }
+  if (!isSanityImageReference(data)) {
+    await Promise.all(Object.values(data).map(loadSanityBlurredPlaceholders));
+    return;
+  }
+
+  const blurredURL = urlBuilder
+    .image(data)
+    .height(30)
+    .quality(30)
+    .format("jpg")
+    .url();
+  const response = await fetch(blurredURL);
+  const blob = await response.arrayBuffer();
+  const base64 = Buffer.from(blob).toString("base64");
+  const contentType = response.headers.get("content-type");
+  const blurredDataURL = `data:${contentType};base64,${encodeURIComponent(
+    base64
+  )}`;
+  (data as SanityImageReference).blurDataURL = blurredDataURL;
 }
