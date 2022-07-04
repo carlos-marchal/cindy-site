f5252bcd54ec911951d47bccb3ea00ab9ce600e5
Carlos Marchal <marchal.carlos@pm.me>
2022-07-04 17:07:34 +0200

    removed debugging error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ErrorBoundary, FallbackProps } from "react-error-boundary";
-
-const ErrorLogger: React.ComponentType<FallbackProps> = (props) => (
-  <pre>{JSON.stringify(props.error, undefined, 2)}</pre>
-);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ErrorBoundary FallbackComponent={ErrorLogger}>
-      <Component {...pageProps} />
-    </ErrorBoundary>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
