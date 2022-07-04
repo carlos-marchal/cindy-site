954e50d9837e9bb4b366e2f72e87a90d0c933d9d
Carlos Marchal <marchal.carlos@pm.me>
2022-07-04 16:50:18 +0200

    added logging error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+
+const ErrorLogger: React.ComponentType<FallbackProps> = (props) => (
+  <pre>{JSON.stringify(props.error, undefined, 2)}</pre>
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary FallbackComponent={ErrorLogger}>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
 
 export default MyApp;
