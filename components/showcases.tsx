237e7ed045912f0971fb13bda1ff4542d1256ea7
Carlos Marchal <marchal.carlos@pm.me>
2022-07-05 20:02:22 +0200

    made showcase grid grow with viewport while preserving aspect ratio

--- components/showcases.tsx
+++ components/showcases.tsx
@@ -17,10 +17,19 @@ export interface ShowcaseThumbnailData {
   cover: SanityImageReference;
 }
 
-const A = styled.a`
-  width: var(--column-width);
-  height: var(--row-height);
+const LI = styled.li`
+  background: red;
+  box-sizing: content-box;
+  padding-top: calc(0.7 * 100%);
   position: relative;
+`;
+
+const A = styled.a`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: block;
 `;
 
@@ -32,13 +41,18 @@ const ShowcaseThumbnail = (props: ShowcaseThumbnailProps) => {
   const showcase = props.children;
   const imageProps = sanityImageProps(showcase.cover, "fill");
   return (
-    <li>
+    <LI>
       <Link href={`/works/${showcase.slug}`} passHref>
         <A>
-          <Image {...imageProps} alt={showcase.title} objectFit="cover" />
+          <Image
+            {...imageProps}
+            alt={showcase.title}
+            objectFit="cover"
+            sizes="(min-width: 1800px) 25vw, (min-width: 1400px) 33vw, (min-width: 1000px) 50vw, 100vw"
+          />
         </A>
       </Link>
-    </li>
+    </LI>
   );
 };
 
@@ -48,12 +62,10 @@ const UL = styled.ul`
   margin: 0 var(--lateral-margin);
   justify-content: center;
   --column-width: 300px;
-  --row-height: 210px;
-  grid-template-columns: repeat(auto-fill, var(--column-width));
+  grid-template-columns: repeat(auto-fill, minmax(var(--column-width), 1fr));
   @media (min-width: 768px) {
-    gap: 40px 30px;
     --column-width: 385px;
-    --row-height: 270px;
+    gap: 40px;
   }
 `;
 
