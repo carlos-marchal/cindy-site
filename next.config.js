d223ec82c6a67e08b56533abe25e81f173f7c3e9
Carlos Marchal <marchal.carlos@pm.me>
2022-07-05 17:52:16 +0200

    added locale and language tags

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: { styledComponents: true },
   images: { domains: ["cdn.sanity.io"] },
+  i18n: { defaultLocale: "en", locales: ["en"] },
 };
 
 module.exports = nextConfig;
