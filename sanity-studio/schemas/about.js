b4ea57c82ce22852367e0d32109ec444795d0f88
Carlos Marchal <marchal.carlos@pm.me>
2022-07-06 22:55:47 +0200

    added links to about page text

--- sanity-studio/schemas/about.js
+++ sanity-studio/schemas/about.js
@@ -43,7 +43,6 @@ export default {
                   lists: [],
                   marks: {
                     decorators: [{ title: "Emphasis", value: "em" }],
-                    annotations: [],
                   },
                 },
               ],
