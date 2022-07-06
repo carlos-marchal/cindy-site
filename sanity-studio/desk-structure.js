96e108c0a9d76d78a8b4b215dee1b482143c112a
Carlos Marchal <marchal.carlos@pm.me>
2022-07-06 19:48:51 +0200

    centralized singleton schema logic

--- sanity-studio/desk-structure.js
+++ sanity-studio/desk-structure.js
@@ -1,30 +1,16 @@
 import S from "@sanity/desk-tool/structure-builder";
+import { singletons } from "./singletons";
 
 export default () =>
   S.list()
     .title("Content")
     .items([
-      S.listItem()
-        .title("Home Page")
-        .child(S.document().schemaType("home").documentId("home")),
-      S.listItem()
-        .title("Works Page")
-        .child(S.document().schemaType("works").documentId("works")),
-      S.listItem()
-        .title("Contact Page")
-        .child(S.document().schemaType("contact").documentId("contact")),
-      S.listItem()
-        .title("About Page")
-        .child(S.document().schemaType("about").documentId("about")),
-      ...S.documentTypeListItems().filter(
-        (listItem) =>
-          !["site_settings", "home", "works", "contact", "about"].includes(
-            listItem.getId()
-          )
+      ...Array.from(singletons).map(([id, name]) =>
+        S.listItem()
+          .title(name)
+          .child(S.document().schemaType(id).documentId(id))
       ),
-      S.listItem()
-        .title("Site Settings")
-        .child(
-          S.document().schemaType("site_settings").documentId("site_settings")
-        ),
+      ...S.documentTypeListItems().filter(
+        (listItem) => !singletons.has(listItem.getId())
+      ),
     ]);
---
+++ sanity-studio/singletons.js
@@ -0,0 +1,7 @@
+export const singletons = new Map([
+  ["site_settings", "Site Settings"],
+  ["home", "Home Page"],
+  ["works", "Works Page"],
+  ["contact", "Contact Page"],
+  ["about", "About Page"],
+]);
