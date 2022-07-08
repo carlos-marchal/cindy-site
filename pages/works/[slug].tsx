267e91d60a7b8b240f2989d8f32150cd75c65e2d
Carlos Marchal <marchal.carlos@pm.me>
2022-07-08 21:07:16 +0200

    removed debugging statement

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -119,7 +119,6 @@ const RelatedWorksHeader = styled.header`
 
 const ShowcasePage: NextPage<ShowcaseProps> = (props) => {
   const [settings, data] = useSanityData(props);
-  console.log(data.slug);
   return (
     <Fragment key={data.slug}>
       <HeadData
