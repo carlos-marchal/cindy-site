d9f8d86e22aa150c1b7dd3f562f57ff6f9c9a8df
Carlos Marchal <marchal.carlos@pm.me>
2022-07-09 12:26:05 +0200

    added decorative gradient following mouse in home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { motion, useMotionValue, useSpring } from "framer-motion";
 import type { GetStaticProps, NextPage } from "next";
 import { groq } from "next-sanity";
+import { useEffect } from "react";
 import styled from "styled-components";
 import { Arrow } from "../components/arrow";
 import { HeadData } from "../components/head-data";
@@ -68,6 +70,7 @@ const IndexPage: NextPage<IndexProps> = (props) => {
         description={settings.description}
         image={settings.preview}
       />
+      <Circle />
       <Header navItems={settings.navigation} />
       <Main>
         {data.main_text}
@@ -77,6 +80,54 @@ const IndexPage: NextPage<IndexProps> = (props) => {
       </Main>
       <Footer>{data.footer}</Footer>
     </Root>
+  );
+};
+
+const SVG = styled(motion.svg)`
+  position: fixed;
+  z-index: -10;
+`;
+
+const Circle = () => {
+  const translateXRaw = useMotionValue(300);
+  const translateYRaw = useMotionValue(300);
+  useEffect(() => {
+    let animating = false;
+    const listener = (event: MouseEvent) => {
+      if (!animating) {
+        animating = true;
+        requestAnimationFrame(() => {
+          translateXRaw.set(event.pageX);
+          translateYRaw.set(event.pageY);
+          animating = false;
+        });
+      }
+    };
+    window.document.body.addEventListener("mousemove", listener);
+    () => window.document.body.removeEventListener("mousemove", listener);
+  }, []);
+  const translateX = useSpring(translateXRaw, { stiffness: 100, damping: 10 });
+  const translateY = useSpring(translateYRaw, { stiffness: 100, damping: 10 });
+  return (
+    <SVG
+      viewBox="0 0 10 10"
+      width="500"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ translateX, translateY, top: -250, left: -250 }}
+    >
+      <defs>
+        <radialGradient id="gradient">
+          <motion.stop
+            initial={{ offset: "20%" }}
+            animate={{ offset: "70%" }}
+            transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
+            stopColor="rgba(218,255,1,1)"
+          />
+          <motion.stop offset="100%" stopColor="rgba(218,255,1,0)" />
+        </radialGradient>
+      </defs>
+      <circle cx="5" cy="5" r="5" fill="url('#gradient')" />
+    </SVG>
   );
 };
 
